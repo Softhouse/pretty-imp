df246068e1e6007a0bd1078f9a5dafa9c293f95d
Ludwig Blaesild <ludwig.blaesild@softhouse.se>
2015-03-27 15:25:22 +0100

    Added html template and run script

--- main.go
+++ main.go
@@ -1,10 +1,17 @@
 package main
 
 import (
-	"log"
+	"html/template"
 	"net/http"
+	"path"
+	"log"
 	"time"
 )
+
+type Profile struct {
+	Name    string
+	Hobbies []string
+}
 
 func timeHandler(w http.ResponseWriter, r *http.Request) {
 	tm := time.Now().Format(time.RFC1123)
@@ -12,13 +19,33 @@ func timeHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-	mux := http.NewServeMux()
+	//mux := http.NewServeMux()
 
 	// Convert the timeHandler function to a HandleFunc type
-	th := http.HandlerFunc(timeHandler)
+	//th := http.HandlerFunc(foo)
 	// And add it to the ServeMux
-	mux.Handle("/", th)
+	//mux.Handle("/", th)
+
+
 
 	log.Println("Listening...")
-	http.ListenAndServe(":3000", mux)
+	http.HandleFunc("/", foo)
+	http.ListenAndServe(":3000", nil)
+}
+
+func foo(w http.ResponseWriter, r *http.Request) {
+	profile := Profile{"Gang!", []string{"snowboarding", "programming"}}
+
+	fp := path.Join("templates", "index.html")
+	tmpl, err := template.ParseFiles(fp)
+	if err != nil {
+		log.Println("Error: " + err.Error())
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	if err := tmpl.Execute(w, profile); err != nil {
+		log.Println("Error: " + err.Error())
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
 }
