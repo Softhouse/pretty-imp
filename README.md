c81e36af826d1a3699959d3521385411db41b870
Fredrik Henrysson <fredrik.henrysson@softhouse.se>
2015-03-27 16:23:23 +0100

    Add route for fetching specific user

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # pretty-imp
+
+# Dependencies
+ * github.com/gorilla/mux
+
+ Installed by running go get github.com/gorilla/mux
--- main.go
+++ main.go
@@ -2,10 +2,13 @@ package main
 
 import (
 	"html/template"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"path"
 	"time"
+
+	"github.com/gorilla/mux"
 )
 
 type Profile struct {
@@ -21,25 +24,22 @@ func timeHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-	//mux := http.NewServeMux()
-
-	// Convert the timeHandler function to a HandleFunc type
-	//th := http.HandlerFunc(foo)
-	// And add it to the ServeMux
-	//mux.Handle("/", th)
-
+	routing := mux.NewRouter()
+	routing.HandleFunc("/{uid:[a-zA-Z]+}", showUser)
+	routing.HandleFunc("/", showMe)
+	http.Handle("/", routing)
 	log.Println("Listening...")
-	http.HandleFunc("/", foo)
 	err := http.ListenAndServe(":3000", nil)
 	if err != nil {
 		log.Println("Failed to start server")
+		return
 	}
 }
 
 func foo(w http.ResponseWriter, r *http.Request) {
 	profile := Profile{"John Doe",
-	"john@example.com",
-	"We are awesome",[]string{"snowboarding", "programming"}}
+		"john@example.com",
+		"We are awesome", []string{"snowboarding", "programming"}}
 
 	fp := path.Join("templates", "index.html")
 	tmpl, err := template.ParseFiles(fp)
@@ -53,4 +53,19 @@ func foo(w http.ResponseWriter, r *http.Request) {
 		log.Println("Error: " + err.Error())
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
+}
+
+func showUser(w http.ResponseWriter, r *http.Request) {
+	personalInfoUrl := "http://xyz.softhouse.se/employees/"
+	params := mux.Vars(r)
+	user := params["uid"]
+	result, err := http.Get(personalInfoUrl + user)
+	if err != nil {
+		w.Write([]byte("Internal Server Error user not found"))
+	}
+	body, err := ioutil.ReadAll(result.Body)
+	if err != nil {
+		w.Write([]byte("Internal Server Error"))
+	}
+	w.Write([]byte(body))
 }
